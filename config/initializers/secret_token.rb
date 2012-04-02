2ad340ff7128d4c8f6fcc25a41b0fd76a41b5782
Ivan <ivanp@kth.se>
2012-04-02 15:00:34 +0200

    Created the rails application framework
    Signed-off-by: Ivan <ivanp@kth.se>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ATBIP::Application.config.secret_token = '81ea7eda5da323c8bb532f7d04aaa6f30a1698da76ca0e527fb35aed0fa0396940d16b6b0536d328115e307d8d46d1c4bb700b25224a546e92fc13a739d742b1'
